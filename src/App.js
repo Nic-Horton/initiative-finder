1f4f32629e2607f88978cc78824db5b8d19c2353
Nic-Horton <nic.horton00@gmail.com>
2023-10-31 18:33:51 -0500

    Added route for initiative tracker page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Crud from './Component/Crud';
 import { Routes, Route } from 'react-router-dom';
+import InitiativeTracker from './Pages/InitiativeTracker';
 
 function App() {
 	return (
 		<div className="App">
 			<Routes>
 				<Route path="/" element={<Crud />}></Route>
+				<Route path="/tracker" element={<InitiativeTracker />}></Route>
 			</Routes>
 		</div>
 	);
