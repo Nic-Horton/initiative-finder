cd15c3881d406dd1c7b1d56a13cc1db9590290b2
lelarry26 <134121139+lelarry26@users.noreply.github.com>
2023-10-31 23:21:30 -0400

    Updating with basic navbar component and partially mapped to  pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import Crud from './Component/Crud';
+import Navbar from "./Component/Navbar";
+import Dashboard from "./Pages/Dashboard";
+import InitiativeTracker from "./Pages/InitiativeTracker";
+import Login from "./Pages/Login";
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
 	return (
-		<div className="App">
-			<Routes>
-				<Route path="/" element={<Crud />}></Route>
-			</Routes>
-		</div>
-	);
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Crud />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/initiativetracker" element={<InitiativeTracker />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Component/Delete.jsx
@@ -0,0 +1,123 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { NavLink } from "react-router-dom";
+
+const drawerWidth = 240;
+const navItems = [
+  { name: "Home", path: "/" },
+  { name: "Characters", path: "/characters" },
+  { name: "Spells", path: "/spells" },
+  { name: "QOTD", path: "/qotd" },
+];
+
+function Navbar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2, fontFamily: "Lugrasimo" }}>
+        Marauder's Memoir
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.name} disablePadding>
+            <ListItemButton
+              component={NavLink}
+              to={item.path}
+              sx={{ textAlign: "center" }}
+            >
+              <ListItemText primary={item.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{
+              fontFamily: "Lugrasimo",
+              flexGrow: 1,
+              display: { xs: "none", sm: "block", textAlign: "left" },
+            }}
+          >
+            Marauder's Memoir
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <Button
+                component={NavLink}
+                to={item.path}
+                key={item.name}
+                sx={{ color: "text.primary" }}
+              >
+                {item.name}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      <Toolbar />
+    </Box>
+  );
+}
+
+export default Navbar;
