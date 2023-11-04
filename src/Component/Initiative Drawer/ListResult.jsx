dff726673cede8c4da922242f352fdc6aae92d78
Nicolas Horton <nic.horton00@gmail.com>
2023-11-03 20:43:50 -0500

    Added layout for button to see more details before adding combatant to tracker and button to add combatant to tacker from the list in the drawer

--- src/Component/Initiative Drawer/ListResult.jsx
+++ src/Component/Initiative Drawer/ListResult.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
 import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 
 function ListResult({combatant}) {
   return (
     <Box width='100%' sx={{borderBottom:'solid 1px'}}>
-    <ListItem key={combatant.id}>
+    <ListItem key={combatant.id}
+      secondaryAction={
+        <IconButton aria-label="info">
+        <QuestionMarkIcon/>
+        </IconButton>
+      }
+    >
+      <ListItemButton>
       <ListItemText primary={combatant.name}/>
+      </ListItemButton>
     </ListItem>
     </Box>
   )
