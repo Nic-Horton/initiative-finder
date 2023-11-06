5c58196515c4bb5e8c5c6f740fcf43ae0cdb3afa
lelarry26 <134121139+lelarry26@users.noreply.github.com>
2023-11-06 01:07:22 -0500

    Updating button modals for more functionality.
    Organized imports based on order in code. Adjusted modal size and grid layout for condition for easier viewing. Added overflow for scrolling of conditions modal. Added dropdown for values of severity for conditions and buffs. Populated status modal with selected conditions and buffs. Added functionality for a persistent switch

--- src/Component/InitiativeOrder/InitiativeOrderCard.jsx
+++ src/Component/InitiativeOrder/InitiativeOrderCard.jsx
@@ -1,28 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Accordion from "@mui/material/Accordion";
-import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
-import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
-
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Box from "@mui/material/Box";
 import ConditionsButton from "./ConditionsButton";
 import BuffsButton from "./BuffsButton";
-import BonusButton from "./StatusButton";
+import StatusButton from "./StatusButton";
 import { amber } from "@mui/material/colors";
 import { lightBlue } from "@mui/material/colors";
-import Grid from "@mui/material/Grid";
+
 const accordionTop = lightBlue[100];
 const accordionDrop = amber[500];
 
 export default function InitiativeOrderCard() {
   const [expanded, setExpanded] = React.useState(false);
+  const [statusValues, setStatusValues] = useState([]);
 
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
+  };
+  //For status to be added to statusValues state array. If the status is already in the array, it will filter out the matching status and remove from array. Else, it will add the status to the array. When switch it toggled "on" it should do the else statement.
+  const handleStatusToggle = (status) => {
+    if (statusValues.includes(status)) {
+      setStatusValues(
+        statusValues.filter((selectedStatus) => selectedStatus !== status)
+      );
+    } else {
+      setStatusValues([...statusValues, status]);
+    }
   };
 
   return (
@@ -30,7 +41,6 @@ export default function InitiativeOrderCard() {
       <React.Fragment>
         <CssBaseline />
         <Container>
-          {/* Accordion only */}
           <Accordion
             expanded={expanded === "panel1"}
             onChange={handleChange("panel1")}
@@ -47,6 +57,7 @@ export default function InitiativeOrderCard() {
               <Typography sx={{ width: "33%", flexShrink: 0 }}>
                 Character Constitution
               </Typography>
+              {/* Character Points */}
               <Grid container alignItems="center">
                 <Grid item xs={2}>
                   <Typography sx={{ color: "text.secondary" }}>HP</Typography>
@@ -72,26 +83,34 @@ export default function InitiativeOrderCard() {
                 </Grid>
               </Grid>
             </AccordionSummary>
+            {/* Dropdown Buttons */}
             <AccordionDetails sx={{ backgroundColor: accordionDrop }}>
               <Grid container spacing={2}>
                 <Grid item xs={12}>
-                  <Grid container spacing={2}>
-                    <Grid item xs={4}>
-                      <ConditionsButton />
+                  <Grid container spacing={1}>
+                    <Grid item xs={3}>
+                      <ConditionsButton
+                        statusValues={statusValues}
+                        handleStatusToggle={handleStatusToggle}
+                        statusColor="Conditions"
+                      />
                     </Grid>
-                    <Grid item xs={4}>
-                      <BuffsButton />
+                    <Grid item xs={3}>
+                      <BuffsButton
+                        statusValues={statusValues}
+                        handleStatusToggle={handleStatusToggle}
+                        statusColor="Buffs"
+                      />
                     </Grid>
-                    <Grid item xs={4}>
-                      <BonusButton />
+                    <Grid item xs={3}>
+                      <StatusButton statusValues={statusValues} />
                     </Grid>
                   </Grid>
                 </Grid>
               </Grid>
             </AccordionDetails>
           </Accordion>
-          {/* Accordion only */}
-          <Box sx={{ bgcolor: "#cfe8fc", height: "50vh" }} />
+          <Box sx={{ bgcolor: "#cfe8fc", height: "50vh", zIndex: -1 }} />
         </Container>
       </React.Fragment>
     </>
--- src/Component/InitiativeOrder/StatusButton.jsx
+++ src/Component/InitiativeOrder/StatusButton.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
 import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
 
-export default function BonusButton() {
+export default function StatusButton({ statusValues }) {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -19,7 +19,7 @@ export default function BonusButton() {
           startIcon={<RocketLaunchIcon />}
           onClick={handleOpen}
         >
-          Bonuses
+          Statuses
         </Button>
         <Modal
           open={open}
@@ -41,10 +41,17 @@ export default function BonusButton() {
             }}
           >
             <Typography id="modal-modal-title" variant="h6" component="h2">
-              Test
+              Selected Status Effects
             </Typography>
             <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              Testtesttest
+              {/* Ternary for if statusValues is = then there are no status effects selected message displayed. Else, the modal is populated with the status */}
+              {statusValues.length === 0 ? (
+                <div>No status effects selected.</div>
+              ) : (
+                statusValues.map((status, index) => (
+                  <div key={index}>{status}</div>
+                ))
+              )}
             </Typography>
           </Box>
         </Modal>
