9ab2c4b55cd4e88ace40b3e365d3bd90cfcf9ec1
Derek Szeto <119750022+MidnightMicro@users.noreply.github.com>
2023-11-07 16:08:14 -0500

    cleaned up code

--- src/Component/Register.js
+++ src/Component/Register.js
@@ -18,72 +18,6 @@ import CloseIcon from '@mui/icons-material/Close';
 import { auth } from "../Config/firebase-config";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
-// function PositionedSnackBar(){
-//   const [state, setState] = React.useState({
-//     open: false,
-//     vertical: 'top',
-//     horizontal: 'center',
-//   });
-//   const { vertical, horizontal, open } = state;
-
-//   const handleClick = (newState) => () => {
-//     setState({ ...newState, open: true });
-//   };
-
-//   const handleClose = () => {
-//     setState({ ...state, open: false });
-//   };
-
-//   const buttons = (
-//     <React.Fragment>
-//       <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-//         <Button onClick={handleClick({ vertical: 'top', horizontal: 'center' })}>
-//           Top-Center
-//         </Button>
-//       </Box>
-//       <Grid container justifyContent="center">
-//         <Grid item xs={6}>
-//           <Button onClick={handleClick({ vertical: 'top', horizontal: 'left' })}>
-//             Top-Left
-//           </Button>
-//         </Grid>
-//         <Grid item xs={6} textAlign="right">
-//           <Button onClick={handleClick({ vertical: 'top', horizontal: 'right' })}>
-//             Top-Right
-//           </Button>
-//         </Grid>
-//         <Grid item xs={6}>
-//           <Button onClick={handleClick({ vertical: 'bottom', horizontal: 'left' })}>
-//             Bottom-Left
-//           </Button>
-//         </Grid>
-//         <Grid item xs={6} textAlign="right">
-//           <Button onClick={handleClick({ vertical: 'bottom', horizontal: 'right' })}>
-//             Bottom-Right
-//           </Button>
-//         </Grid>
-//       </Grid>
-//       <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-//         <Button onClick={handleClick({ vertical: 'bottom', horizontal: 'center' })}>
-//           Bottom-Center
-//         </Button>
-//       </Box>
-//     </React.Fragment>
-//   );
-
-//   return (
-//     <Box sx={{ width: 500 }}>
-//       {buttons}
-//       <Snackbar
-//         anchorOrigin={{ vertical, horizontal }}
-//         open={open}
-//         onClose={handleClose}
-//         message="I love snacks"
-//         key={vertical + horizontal}
-//       />
-//     </Box>
-//   );
-// }
 
 function BottomAlert ({ open, severity, message }) {
   return (
@@ -96,14 +30,10 @@ function BottomAlert ({ open, severity, message }) {
 export default function RegisterPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  // const [displayName, setDisplayName] = useState("")
   const [open, setOpen] = useState(false);
   const [alertMessage, setAlertMessage] = useState('');
   const [alertSeverity, setAlertSeverity] = useState('info');
   const [showAlert, setShowAlert] = useState(false);
-  // useState (false) show div to  populate error 
-  // useState takes place of if/else statements
-  // trigger use state 
 
   const showBottomAlert = () => {
     return (
@@ -120,10 +50,6 @@ export default function RegisterPage() {
   const handleShowAlertClickO = () => setShowAlert(true)
   const handleShowAlertClickC= () => setShowAlert(false)
   
-
-// const autoHideDuration = () => {
-//   autoHideDuration=500;
-// }
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -145,12 +71,6 @@ export default function RegisterPage() {
       setTimeout(()=> { 
         setOpen(false);
       }, 3000);
-      
-
-      // You can also hide the alert after a certain duration (e.g., 3 seconds)
-      // setTimeout(() => {
-      //   setShowAlert(false);
-      // }, 3000);
     } catch (err) {
       console.error(err);
 
@@ -182,70 +102,7 @@ export default function RegisterPage() {
       }
     }
   };
-  // const handleCloseReg = async () => {
-  //   try {
-  //     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-  //     const user = userCredential.user;
-  //     // alert("Critical Success! User Created!")
-  //     console.log("User Created!");
-  //     setAlertSeverity('success');
-  //     setAlertMessage('Critical Success!')
-  //     setOpen(true);
-  //   } catch (err) {
-  //     console.error(err)
-  //     if (err) {
-  //       try {
-  //         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-  //         const user = userCredential.user;
-  //         console.log("User created:", user);
-  //         setAlertSeverity('success');
-  //         setAlertMessage('Critical Success! User Created!');
-  //         setShowAlert(true);
-  //       } catch (createErr) {
-  //         console.error("Error creating user:", createErr);
-  //       }
-  //     } else {
-  //       setAlertSeverity('error');
-  //       setAlertMessage('Error signing in: ' + err.message);
-  //       setShowAlert(true);
-  //       console.error("Error signing in:", err);
-  //     }
-  //       // User doesn't exist, you can proceed to create a new user
-  //       // work on  errors
-  //       // Validation? 
-  //       // hidden or display:none
-  //       // css for display open
-  //     //   return (
-  //     //   <Alert
-  //     //   open={open}
-  //     //   severity={alertSeverity}
-  //     //   onClose={() => setOpen(false)}
-  //     // >
-  //     //   <AlertTitle>{alertSeverity === 'success' ? 'Success' : 'Error'}</AlertTitle>
-  //     //   {alertMessage}
-  //     // </Alert>
-  //     //   )
-  //     }
 
-  //   //   if (!err) {
-  //   //     try {
-  //   //       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-  //   //       const user = userCredential.user;
-  //   //       console.log("User created:", user);
-  //   //       setOpen(true);
-  //   //       alert("Critical Success!");
-  //   //     } catch (createErr) {
-  //   //       console.error("Error creating user:", createErr);
-  //   //     }
-  //   //   } else {
-  //   //     setAlertSeverity('error');
-  //   //     setAlertMessage('Error signing in');
-  //   //     setOpen(true);
-  //   //     // alert("Error signing in: Email already in use", err)
-  //   //     console.error("Error signing in:", err);
-  //   //   }
-  //   // }
-  // };
   return (
     <> 
     <div>
