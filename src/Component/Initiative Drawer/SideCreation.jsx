6f42c6db3ff0df4b6abbeee03d1e3850fd7e9382
Nicolas Horton <nic.horton00@gmail.com>
2023-11-13 14:30:43 -0600

    Added style to preview modal of combatants

--- src/Component/Initiative Drawer/SideCreation.jsx
+++ src/Component/Initiative Drawer/SideCreation.jsx
@@ -185,7 +185,7 @@ export default function SideCreation({ combatantList, setCombatantList }) {
           border: 5,
           backgroundColor: "rgba(38, 50, 56,0.75)" ,
           borderColor: "rgba(200,184,116)",
-          borderRadius: 10,
+          borderRadius: 6,
           mt: 2,
           width: "97%",
         }}
